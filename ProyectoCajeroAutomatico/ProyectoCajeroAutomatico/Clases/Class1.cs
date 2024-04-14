74157b2dde29c1e337e823c7f3ab2619e84d974c
Lautaro Sanchez Chandia <lautarosanche@gmail.com>
2024-04-14 20:44:38 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoCajeroAutomatico/ProyectoCajeroAutomatico/Clases/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCajeroAutomatico.Clases
+{
+    internal class Class1
+    {
+        public string hola;
+
+        public Class1() { }
+        public Class1(string hola) { 
+        
+        }
+    }
+}
---
+++ ProyectoCajeroAutomatico/ProyectoCajeroAutomatico/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoCajeroAutomatico.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCajeroAutomatico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+        }
+    }
+}
